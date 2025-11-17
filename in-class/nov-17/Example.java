4141057aa17439f5ae4debe5f98256e358bd6a8d
c212 <40395648+c212@users.noreply.github.com>
2025-11-17 11:56:28 -0500

    stage 02

--- in-class/nov-17/Example.java
+++ in-class/nov-17/Example.java
@@ -4,9 +4,17 @@ public class Example {
   public static void main(String[] args) {
     Model m = new Model(args);
     for (int i = 0; i < args.length; i++) {
-      System.out.println("Creating View and Controller for: " + args[i]);
+      System.out.println("Processing " + args[i]);
+      View v = new View(m, i); 
+
+      JFrame f = new JFrame("Player: " + args[i]); 
+      f.setSize(400, 400); 
+      f.add(v);
+      
+      f.setVisible(true);      
     }
   }
 }
 
+// --( Example.java )--
 // --( Example.java )--
